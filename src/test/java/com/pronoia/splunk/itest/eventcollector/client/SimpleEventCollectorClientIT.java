50fbf1fb07406503fbb1c83b02d1f766acb098c1
Quinn Stevenson <quinn@pronoia-solutions.com>
2017-06-22 10:32:24 -0600

    Staging for 0.1.1-SNAPSHOT

--- src/test/java/com/pronoia/splunk/itest/eventcollector/client/SimpleEventCollectorClientIT.java
+++ src/test/java/com/pronoia/splunk/itest/eventcollector/client/SimpleEventCollectorClientIT.java
@@ -19,32 +19,50 @@ package com.pronoia.splunk.itest.eventcollector.client;
 
 import com.pronoia.splunk.eventcollector.client.SimpleEventCollectorClient;
 
+import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Integration tests for the SimpleEventCollectorClient.
  */
 public class SimpleEventCollectorClientIT {
+  Logger log = LoggerFactory.getLogger(this.getClass());
+
   SimpleEventCollectorClient client;
 
   static void setClientInfo(SimpleEventCollectorClient genericClient) {
-    genericClient.setHost("localhost");
     genericClient.setPort(8088);
+
+    genericClient.setHost("localhost");
     genericClient.setAuthorizationToken("11ABA1B0-CF99-4E8A-8131-A162CBB32163");
-    // genericClient.disableCertificateValidation();
+    genericClient.disableCertificateValidation();
   }
 
   @Before
   public void setUp() throws Exception {
     client = new SimpleEventCollectorClient();
+    setClientInfo(client);
   }
 
   /**
+   * As long as the sendEvent method succeeds, this test passes.  Manual validation of the payload in Splunk is
+   * required.
+   *
    * @throws Exception in the event of a test error.
    */
   @Test
   public void testPostPayload() throws Exception {
-    client.sendEvent("{\"event\":\"Hello\"}");
+    JSONObject payloadBuilder = new JSONObject();
+
+    // payloadBuilder.put("index", "main");
+    payloadBuilder.put("event", "Hello");
+
+    String payload = payloadBuilder.toJSONString();
+
+    client.sendEvent(payload);
   }
+
 }
